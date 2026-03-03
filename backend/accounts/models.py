616a25054017933ebae7b35f7b48e4d637ff7f09
SzymanskiJacek <92018118+SzymanskiJacek@users.noreply.github.com>
2026-03-03 14:08:09 +0100

    Update models.py

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -5,8 +5,9 @@ class SavedCalculator(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     name = models.CharField(max_length=120)
     data = models.JSONField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField()
 
     def __str__(self):
         return f"{self.name} (user_id={self.user.email})"
+
 
