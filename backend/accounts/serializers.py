1b5396c9b6a202fb447f4232f9e1af05ee9d6028
SzymanskiJacek <92018118+SzymanskiJacek@users.noreply.github.com>
2026-03-03 12:29:34 +0100

    Update serializers.py

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -100,10 +100,11 @@ class UsersSerializer(serializers.ModelSerializer):
 
 class SavedCalculatorsSerializer(serializers.ModelSerializer):
     user_email = serializers.EmailField(source='user.email')
-    user_id = serializers.EmailField(source='user.id')
+    user_id = serializers.IntegerField(source='user.id')
     class Meta:
         model = SavedCalculator
-        fields = ['id', 'user_email', 'name', 'created_at, 'user_id']
+        fields = ['id', 'user_email', 'name', 'created_at', 'user_id']
+
 
 
 
